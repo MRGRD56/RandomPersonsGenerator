39d5f31d5ebe97bf130a6316d07b339016d23130
MRGRD56 <kirgol85@gmail.com>
2021-05-25 14:49:13 +0500

    Initial Commit

---
+++ RdtTestApp/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using HandyControl.Controls;
+
+namespace RdtTestApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : BlurWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ RdtTestApp/MainWindowViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using RandomDataToolsInterop;
+using RandomDataToolsInterop.Models;
+using RdtTestApp.Annotations;
+
+namespace RdtTestApp
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private int _progressValue;
+        public ObservableCollection<Person> People { get; } = new();
+        
+        public MainWindowViewModel()
+        {
+            LoadData();
+        }
+
+        public int ProgressValue
+        {
+            get => _progressValue;
+            set
+            {
+                _progressValue = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private async void LoadData()
+        {
+            var syncContext = SynchronizationContext.Current;
+
+            ProgressValue = 0;
+            for (var i = 0; i < 10; i++)
+            {
+                var people = await Api.GetPeopleAsync(100);
+                await Task.Run(() =>
+                {
+                    people.ForEach(person =>
+                    {
+                        syncContext.Send(_ =>
+                        {
+                            People.Add(person);
+                            ProgressValue++;
+                        }, null);
+                    });
+                });
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
